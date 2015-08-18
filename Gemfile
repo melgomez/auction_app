97f5e27c0eb5a328e2248d0ad1cc6faa18819d71
melgomez <meldarrelgomez@gmail.com>
2015-08-18 21:48:43 +0800

    Integrate user authentication app.

--- Gemfile
+++ Gemfile
@@ -44,7 +44,7 @@ group :development, :test do
   gem 'guard-rspec'
   gem 'database_cleaner', '~> 1.4.1'
 end
-
+gem 'rspec-rails', group: [:test, :development]
 group :test do
   gem 'factory_girl_rails', '~>4.0'
   gem 'capybara'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  
+  EMAIL_REGEX = /\A[a-z0-9._%+=]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  
+  validates_presence_of :first_name
+  validates_length_of :first_name, :maximum => 25
+  
+  validates_presence_of :last_name
+  validates_length_of :last_name, :maximum => 50
+  
+  validates_presence_of :username
+  validates_length_of :username, :within => 8..25
+  validates_uniqueness_of :username
+  
+  validates_presence_of :email 
+  validates_length_of :email, :maximum => 100
+  validates_format_of :email, :with => EMAIL_REGEX
+  validates_confirmation_of :email
+  validates_uniqueness_of :email
+  
+  validates_presence_of :password
+  validates_confirmation_of :password
 end
